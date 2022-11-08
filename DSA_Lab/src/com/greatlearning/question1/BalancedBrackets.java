5d12b27925ede54eaddb8229d47517499addcde6
ankitjha15 <114355852+ankitjha15@users.noreply.github.com>
2022-11-08 23:30:44 +0530

    Add files via upload

---
+++ DSA_Lab/src/com/greatlearning/question1/BalancedBrackets.java
@@ -0,0 +1,47 @@
+package com.greatlearning.question1;
+
+import java.util.Stack;
+
+public class BalancedBrackets {
+	public static void main(String[] args) {
+		String input = "{[[{}]]}";
+		boolean result = isBalancedStringExp(input);
+		if(result) {
+			System.out.println("The entered String has Balanced Brackets.");
+		}else {
+			System.out.println("The entered String does not contain Balanced Brackets.");
+		}
+	}
+	private static boolean isBalancedStringExp(String input) {
+		Stack<Character> data = new Stack<Character>();
+		for(int i=0;i<input.length();i++) {
+			char ch = input.charAt(i);
+			if(ch == '(' || ch == '[' || ch == '{') {
+				data.push(ch);
+				continue;
+			}
+			if(data.isEmpty()) {
+				return false;
+			}
+			char matchCh = data.pop();
+			switch(ch) {
+			case ')':
+				if(matchCh != '(') {
+					return false;
+				}
+				break;
+			case ']':
+				if(matchCh != '[') {
+					return false;
+				}
+				break;
+			case '}':
+				if(matchCh != '{') {
+					return false;
+				}
+				break;
+			}
+		}
+		return data.isEmpty();
+	}
+}
